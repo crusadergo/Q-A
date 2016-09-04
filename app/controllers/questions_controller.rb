d1d6c5c30e66a54ecb17a7f3d668c41bad87bdc5
crusadergo <crusadergo@yandex.ru>
2016-09-05 00:09:12 +0500

    add simple posting

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,2 +1,17 @@
 class QuestionsController < ApplicationController
+  def new
+    question = Question.new
+  end
+
+  def create
+    article = Question.new question_params
+
+    article.save
+    redirect_to root_path
+  end
+
+  private
+    def question_params
+      params.require(:question).permit(:title, :description)
+    end
 end
