9b1769fa7d9f832f890382089e54bf1c74dfd5d0
crusadergo <crusadergo@yandex.ru>
2016-09-07 21:27:32 +0500

    nested routes for q and a

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,9 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'application#index'
 
-  resources :questions
+  resources :questions do
+    resources :answers do
+  end
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
